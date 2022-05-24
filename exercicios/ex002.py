a1d2e310391b1770ff08d03c05fe322e88e7c4ea
zz-dev <zz-dev@outlook.com>
2022-05-24 19:39:41 -0400

    update

---
+++ exercicios/ex002.py
@@ -0,0 +1,6 @@
+'''
+Faça um Input e Responda o usuário.
+'''
+
+digite_seu_nome = input('Digite seu nome: ')
+print(f'É um prazer te conhecer {digite_seu_nome}!')
---
+++ exercicios/ex003.py
@@ -0,0 +1,9 @@
+'''
+Faça um input que peça dois valores e some os dois.
+'''
+
+numero1 = int(input('Digite um valor: '))
+numero2 = int(input('Digite outro valor: '))
+resultado = numero1 + numero2
+
+print(f'A soma de: {numero1} + {numero2} é igual: {resultado}')
