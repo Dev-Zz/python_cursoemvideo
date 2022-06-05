2ce5c9dd0732abc42dd7c3999716516ec9165adc
Dev-Zz <zz-dev@outlook.com>
2022-06-05 13:05:07 -0300

    Update

--- exercicios/ex070.py
+++ exercicios/ex070.py
@@ -8,3 +8,30 @@ B) quantos produtos custam mais de R$1000.
 C) qual é o nome do produto mais barato. 
 '''
 
+total = total_mais_mil = 0
+while True: 
+    print('-'*50)
+    produto = str(input('Nome do PRODUTO: ')).upper()
+    preço = float(input('PREÇO: '))
+    total += preço
+    if preço > 1000:
+        total_mais_mil +=1
+    
+    resp = ' '
+    while resp not in 'SN':
+        resp = str(input('Quer CONTINUAR[S/N]: ')).strip().upper()
+        print('-'*50)
+    if resp == 'N':
+        break
+print('-'*50)    
+print('A COMPRA ACABOU')
+print(f'O TOTAL da compra foi: {total:.2f}')
+print(f'{total_mais_mil} produtos custam mais de R$1000.00 ')
+
+print('-'*50)  
+
+
+#if preço > 1000:
+#    quant_produto_mais_mil = quant_produto_mais_mil + 1
+
+
