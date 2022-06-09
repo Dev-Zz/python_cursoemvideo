729505cb55dac91760954769531016cc1b8ac18c
zz-dev <zz-dev@outlook.com>
2022-06-09 19:42:29 -0400

    update

--- exercicios/ex094.py
+++ exercicios/ex094.py
@@ -1,11 +1,19 @@
-'''Crie um programa que leia nome, sexo e idade de várias pessoas, guardando os dados de cada pessoa em um dicionário e todos os dicionários em uma lista. No final, mostre: 
+'''Crie um programa que leia nome, sexo e idade de várias pessoas, guardando os dados de cada 
+pessoa em um dicionário e todos os dicionários em uma lista. No final, mostre: 
 A) Quantas pessoas foram cadastradas
 B) A média de idade
 C) Uma lista com as mulheres
 D) Uma lista de pessoas com idade acima da média'''
 
+from time import sleep
+
+
 dados = {}
 dados_lista = []
+idade = []
+dados_f = []
+acima_da_média = []
+
 print('='*80)
 while True:
     dados.clear()
@@ -15,16 +23,31 @@ while True:
         if dados['sexo'] in 'MF':
             break
         print('ERRO! Por favor, digite M ou F.')
+    if dados['sexo'] == 'F':
+        dados_f.append(dados['nome'])
     dados['idade'] = int(input('Idade: '))
+    if dados['idade'] > 0:
+        idade.append(dados['idade'])
     dados_lista.append(dados.copy())
+    if dados['idade'] > media:
+        acima_da_média.append(dados['nome'])
     while True:
         resp = str(input('Quer continuar[S/N]: ')).upper()[0]
         if resp in 'SN':
             break
         print('ERRO! Responda apenas S ou N.')
+    print('='*80)
     if resp == 'N':
         break
-print('='*80)
-for i, v in enumerate (dados_lista):
-    print(f'{i+1} = {v}')
-print('='*80)    
+media = sum(idade) / len(idade)
+print(f'A média das idades cadastradas é: {media:.2f}')
+print('-'*80)
+for i, v in enumerate(dados_f):
+    sleep(0.5)
+    print(f'A {i+1}º mulher cadastrada foi a {v}')
+print('-'*80)
+for i, v in enumerate(acima_da_média):
+    sleep(0.5)
+    print(f'Pessoa: {v} está acima da média de idade.')
+print('-'*80)
+
